8e9487b5f7dc4df0a2f68c31859ca6844edc43f5
jayansh2008 <73336815+jayansh2008@users.noreply.github.com>
2020-11-14 16:33:11 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,36 +1,66 @@
+var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
+var packageBody,ground
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
+const Body = Matter.Body;
 
-var engine, world;
-var ground, ball;
-function setup(){
-createCanvas(400,400);
-engine = Engine.create();
-world = engine.world;
-
-var ground_options = {
-  isStatic : true
+function preload()
+{
+	helicopterIMG=loadImage("helicopter.png")
+	packageIMG=loadImage("package.png")
 }
 
-ground = Bodies.rectangle(200,390,200,20, ground_options);
-World.add(world, ground); 
+function setup() {
+	createCanvas(800, 700);
+	rectMode(CENTER);
+	
 
-var options = {
-  restitution : 1
+	packageSprite=createSprite(width/2, 80, 10,10);
+	packageSprite.addImage(packageIMG)
+	packageSprite.scale=0.2
+
+	helicopterSprite=createSprite(width/2, 200, 10,10);
+	helicopterSprite.addImage(helicopterIMG)
+	helicopterSprite.scale=0.6
+
+	groundSprite=createSprite(width/2, height-35, width,10);
+	groundSprite.shapeColor=color(255)
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:3, isStatic:true});
+	World.add(world, packageBody);
+	
+
+	//Create a Ground
+	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
+ 	World.add(world, ground);
+
+
+	Engine.run(engine);
+  
 }
 
-ball = Bodies.circle(200, 300, 30, options);
-World.add(world, ball);
 
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  packageSprite.x= packageBody.position.x 
+  packageSprite.y= packageBody.position.y 
+  drawSprites();
+ 
 }
 
-function draw(){
-  Engine.update(engine);
-background(0);
-rectMode(CENTER);
-rect(ground.position.x, ground.position.y, 400, 20);
+function keyPressed() {
+ if (keyCode === DOWN_ARROW) {
+    // Look at the hints in the document and understand how to make the package body fall only on press of the Down arrow key.
 
-ellipseMode(RADIUS);
-ellipse(ball.position.x, ball.position.y,30, 30);
+    
+  }
 }
+
+
+
